c106def3b98f725521f51ee89a9785d3776b236a
Joe Oliveira <joe@joebcc.com>
2021-02-25 09:17:07 -0800

    init

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const svgContents = require("eleventy-plugin-svg-contents");
+
+const CleanCSS = require("clean-css");
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
+    eleventyConfig.addWatchTarget("./src/scss/");
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/static");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    
+    // add naviagtion plugin
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(svgContents);
+
+
+
+
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+            includes: "_includes",
+            layouts: "_layouts",
+        }
+    }
+
+}
